8ed815c4b1166d22911d7402d7fb7ecc2c21fbcb
Victor Peres <victor.peres93@gmail.com>
2018-02-25 13:21:13 -0300

    test: new test and coveralls coverage

--- tests/test_planets.py
+++ tests/test_planets.py
@@ -1,2 +1,7 @@
+from planetsapi.resources.planets import Planets
 def test_1_equal_1():
     assert 1 == 1
+
+def test_get_tatooine():
+    planets = Planets()
+    assert planets.get() == "Tatooine"
